55312c53ca306daa2730a63d2a90446a79054169
AleWaweru <ngashalex8@gmail.com>
2023-08-10 09:20:35 +0300

    dev branch

--- solver.rb
+++ solver.rb
@@ -1,27 +1,21 @@
-
 class Solver
-    def self.factorial(n)
-      raise ArgumentError, 'Input must be a non-negative integer' if n < 0
-  
-      return 1 if n.zero?
-  
-      (1..n).reduce(:*)
-    end
-  
-    def self.reverse(word)
-      word.reverse
-    end
-  
-    def self.fizzbuzz(n)
-      if (n % 3).zero? && (n % 5).zero?
-        'fizzbuzz'
-      elsif (n % 3).zero?
-        'fizz'
-      elsif (n % 5).zero?
-        'buzz'
-      else
-        n.to_s
-      end
+  def self.factorial(number)
+    raise ArgumentError, 'Input must be a non-negative integer' if number.negative?
+
+    return 1 if number.zero?
+
+    (1..number).reduce(:*)
+  end
+
+  def self.fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
     end
   end
-  
+end
