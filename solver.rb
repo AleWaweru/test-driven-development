17851fca9ab98895e2cf3a0d6cc425a40daf383e
AleWaweru <ngashalex8@gmail.com>
2023-08-11 16:39:30 +0300

    Created factorial method and conducted test

--- solver.rb
+++ solver.rb
@@ -1,25 +1,10 @@
 class Solver
-  def self.factorial(number)
-    raise ArgumentError, 'Input must be a non-negative integer' if number.negative?
+  def factorial(num)
+    raise ArgumentError, 'Factorial is not defined for negative integers.' if num.negative?
+    return 1 if num.zero?
 
-    return 1 if number.zero?
-
-    (1..number).reduce(:*)
-  end
-
-  def self.reverse(word)
-    word.reverse
-  end
-
-  def self.fizzbuzz(number)
-    if (number % 3).zero? && (number % 5).zero?
-      'fizzbuzz'
-    elsif (number % 3).zero?
-      'fizz'
-    elsif (number % 5).zero?
-      'buzz'
-    else
-      number.to_s
-    end
+    result = 1
+    (1..num).each { |i| result *= i }
+    result
   end
 end
