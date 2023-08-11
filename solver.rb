9137c31592ad4be31635f16e53a37c0b750621b6
AleWaweru <ngashalex8@gmail.com>
2023-08-11 15:19:58 +0300

    unit test for factorial

--- solver.rb
+++ solver.rb
@@ -1,27 +1,10 @@
-
 class Solver
-    def self.factorial(n)
-      raise ArgumentError, 'Input must be a non-negative integer' if n < 0
-  
-      return 1 if n.zero?
-  
-      (1..n).reduce(:*)
-    end
-  
-    def self.reverse(word)
-      word.reverse
-    end
-  
-    def self.fizzbuzz(n)
-      if (n % 3).zero? && (n % 5).zero?
-        'fizzbuzz'
-      elsif (n % 3).zero?
-        'fizz'
-      elsif (n % 5).zero?
-        'buzz'
-      else
-        n.to_s
-      end
-    end
+  def factorial(num)
+    raise ArgumentError, 'Factorial is not defined for negative integers.' if num.negative?
+    return 1 if num.zero?
+
+    result = 1
+    (1..num).each { |i| result *= i }
+    result
   end
-  
+end
---
+++ spec/solver_spec.rb
@@ -0,0 +1,22 @@
+# In the test file (e.g., solver_spec.rb)
+require_relative '../solver'
+
+describe Solver do
+  let(:solver) { Solver.new } # Create an instance of the Solver class
+
+  describe '#factorial' do
+    it 'returns 1 for 0' do
+      expect(solver.factorial(0)).to eq(1)
+    end
+    it 'returns 1 for 1' do
+      expect(solver.factorial(1)).to eq(1)
+    end
+    it 'returns the correct factorial for positive integers' do
+      expect(solver.factorial(5)).to eq(120)
+    end
+    it 'raises an error for negative integers' do
+      expect { solver.factorial(-3) }.to raise_error(ArgumentError)
+    end
+  end
+
+end
