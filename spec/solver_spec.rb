145373637352ccab974b6334868e4b034de3a4cf
AleWaweru <ngashalex8@gmail.com>
2023-08-11 16:02:04 +0300

    Add reserver testing

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -18,4 +18,28 @@ describe Solver do
       expect { solver.factorial(-3) }.to raise_error(ArgumentError)
     end
   end
+
+  describe '#reverse' do
+    it 'reverses the word' do
+      expect(solver.reverse('hello')).to eq('olleh')
+    end
+    it 'returns an empty string for an empty word' do
+      expect(solver.reverse('')).to eq('')
+    end
+  end
+
+  describe '#fizzbuzz' do
+    it 'returns "fizz" for numbers divisible by 3' do
+      expect(solver.fizzbuzz(3)).to eq('fizz')
+    end
+    it 'returns "buzz" for numbers divisible by 5' do
+      expect(solver.fizzbuzz(5)).to eq('buzz')
+    end
+    it 'returns "fizzbuzz" for numbers divisible by both 3 and 5' do
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+    it 'returns the number as a string for other cases' do
+      expect(solver.fizzbuzz(7)).to eq('7')
+    end
+  end
 end
